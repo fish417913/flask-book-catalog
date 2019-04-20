3df9157cd77954d22a7d7361f4418692f3fa7a20
Mike <aguycalledfish@gmail.com>
2019-04-20 07:13:48 -0400

    errors fixed, maybe

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ with flask_app.app_context():
     db.create_all()
 
     if not User.query.filter_by(user_name='harry').first():
-            User.create_user(user='harry', email='harry@potters.com',password='secret'
+            User.create_user(user='harry', email='harry@potters.com',password='secret')
             flask_app.run()
 
 
