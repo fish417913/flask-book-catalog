c5642012b9441fa4af518f1a24c833868f9b215d
Mike <aguycalledfish@gmail.com>
2019-04-19 22:12:43 -0400

    fixed more errors

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 from app import create_app, db
 from app.auth.models import User
-from sqlalchemy import exc
+
 
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    try:
+
 
         if not User.query.filter_by(user_name='harry').first():
             User.create_user(user='harry',
